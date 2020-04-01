8960fc651f1c1805c573f8c37c1ea1254e0f219b
Administrator <1764773283@qq.com>
2020-04-01 16:42:49 +0800

    主页bug修复

---
+++ src/main/webapp/js/index/dataFormat.js
@@ -0,0 +1,14 @@
+function timestampToTime(value) {
+    date = new Date(value);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var m1 = date.getMinutes();
+    var s = date.getSeconds();
+    m = m < 10 ? ("0" + m) : m;
+    d = d < 10 ? ("0" + d) : d;
+    h = h < 10 ? ("0" + h) : h;
+    m1 = m1 < 10 ? ("0" + m1) : m1;
+    return y + "-" + m + "-" + d;
+}
