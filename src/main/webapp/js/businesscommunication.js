c716d0e21d2c42e6dc81e570d1be16cf35618334
Administrator <1764773283@qq.com>
2020-04-03 13:47:04 +0800

    单页面增加百度统计

---
+++ src/main/webapp/js/businesscommunication.js
@@ -0,0 +1,7 @@
+var  _hmt  =  _hmt  ||  [];
+(function()  {
+    var  hm  =  document.createElement("script");
+    hm.src  =  "https://hm.baidu.com/hm.js?71fd9b050ac55b52d33231e1765d60dc";
+    var  s  =  document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm,  s);
+})();
