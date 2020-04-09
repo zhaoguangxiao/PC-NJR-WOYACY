efcc8f48d90fb7a3646ab62f9a65e71bf3b4c14e
Administrator <1764773283@qq.com>
2020-04-09 15:03:48 +0800

    百度统计代码更改

--- src/main/webapp/js/businesscommunication.js
+++
@@ -1,7 +0,0 @@
-var  _hmt  =  _hmt  ||  [];
-(function()  {
-    var  hm  =  document.createElement("script");
-    hm.src  =  "https://hm.baidu.com/hm.js?71fd9b050ac55b52d33231e1765d60dc";
-    var  s  =  document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm,  s);
-})();
