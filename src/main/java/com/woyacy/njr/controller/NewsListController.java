79d0e0bb0b7e6914c9ee43c39d17a04213f34614
Administrator <1764773283@qq.com>
2020-05-28 16:42:30 +0800

    新闻分页 优化

--- src/main/java/com/woyacy/njr/controller/NewsListController.java
+++ src/main/java/com/woyacy/njr/controller/NewsListController.java
@@ -4,13 +4,17 @@ import com.alibaba.fastjson.JSON;
 import com.woyacy.njr.bean.ComprehensBean;
 import com.woyacy.njr.util.HttpUtil;
 import com.woyacy.njr.util.JsonConvertObject;
+import com.woyacy.njr.util.PageUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Administrator
@@ -24,11 +28,25 @@ public class NewsListController {
 
 
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public String toNewList(Model model) throws IOException {
+    public String toNewList(@RequestParam Map<String, String> map,
+                            Model model) throws IOException {
+
+
         String url1="https://manage.zhou-yuanwai.com/comprehens/journalism.do";
         //查出全部文章
         String result = HttpUtil.sendGet(url1, "?uidpk=20");
-        model.addAttribute("news", JsonConvertObject.toBaseBean(result));
+        List<ComprehensBean> comprehensBeans = JsonConvertObject.toBaseBean(result);
+
+
+        if (map.size() == 0 || map.get("currentPage").isEmpty()) {
+            map.put("currentPage", "0");
+        }
+
+        // 参数为当前页码、每页显示条数、查询的列表集合
+        PageUtil pageInfo = new PageUtil(Integer.valueOf(map.get("currentPage")), 10, comprehensBeans);
+        model.addAttribute("userDTOList", pageInfo.getList());
+        model.addAttribute("pageInfo", pageInfo);
+
         return "news";
     }
 
